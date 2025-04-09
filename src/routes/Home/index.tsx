2d78e6b2f8511156ebc4ec0c38eac1f4b1ce0415
Zach Millar <zach.millar@leaftrade.com>
2025-04-08 20:01:20 -0700

    chore: update home

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,5 +1,13 @@
+import { ContactButton } from "@/components/ContactButton";
 import { FC } from "react";
+import styles from "./Home.module.css";
 
 export const Home: FC = () => {
-  return <>Home</>;
+  return (
+    <div className={styles.home}>
+      <span>Work in Progress</span>
+      <span>Always accepting new clients</span>
+      <ContactButton />
+    </div>
+  );
 };
